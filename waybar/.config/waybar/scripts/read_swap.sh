31f75041de635c70540a07d67f4920438e0558e2
Yurii Bilous <seidzuro@gmail.com>
2026-01-12 20:07:49 +0200

    waybar: update config
    - fix read_swap.sh script
    - fix changing mic level on scroll
    - adjust some colors and formatting

--- waybar/.config/waybar/scripts/read_swap.sh
+++ waybar/.config/waybar/scripts/read_swap.sh
@@ -1,15 +1,3 @@
 #!/bin/sh
 
-swap_info=$(free -m | awk '/Swap/ {print $2, $3}')
-
-swap_total=$(awk '{print $1}')
-swap_used=$(awk '{print $2}')
-
-if [ "$swap_total" -ne 0 ]; then
-    # Calculate the percentage of swap used
-    swap_used_percent=$(( 100 * swap_used / swap_total ))
-else
-    swap_used_percent=0
-fi
-
-echo "$swap_used_percent"
+free -m | awk '/Swap/ { if ($2 > 0) printf "%.0f\n", $3*100/$2; else print 0 }'
